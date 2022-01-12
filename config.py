cd42fcc4b15188c8f2a5b3211782dd17ab124b90
harshithtunuguntla <harshithtunuguntla@gmail.com>
2022-01-13 02:20:17 +0530

    config brought back

---
+++ config.py
@@ -0,0 +1,17 @@
+apiKey= "AIzaSyAUDh0-DJSc74VOv9mSIGULNfWxA0Jxwdo"
+
+authDomain= "imageupload-19537.firebaseapp.com"
+
+projectId= "imageupload-19537"
+
+databaseURL= 'https=//imageupload-19537.firebaseio.com'
+
+storageBucket= "imageupload-19537.appspot.com"
+
+messagingSenderId= "216122971515"
+
+appId= "1=216122971515=web=545cf46083d59030cb19de"
+
+measurementId= "G-S2PCPDGCML"
+
+serviceAccount= "imageupload-19537-firebase-adminsdk-aa50y-d2df163fae.json"
